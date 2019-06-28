02227935e959aaa62b9fad31464a4e676b8611a0
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-28 12:01:33 -0400

    Add new policy document for EC2 permissions needed by packer to create AMIs

--- main.tf
+++ main.tf
@@ -43,3 +43,60 @@ resource "aws_iam_user_policy" "ssm_policy" {
   user   = aws_iam_user.user.id
   policy = data.aws_iam_policy_document.ssm_parameter_doc[count.index].json
 }
+
+# IAM policy documents that allow the EC2 actions needed for packer to create
+# AMIs.  This will be applied to the IAM user we are creating.
+data "aws_iam_policy_document" "ec2_packer_doc" {
+  count = var.add_packer_permissions ? 1 : 0
+
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ec2:AttachVolume",
+      "ec2:AuthorizeSecurityGroupIngress",
+      "ec2:DeregisterImage",
+      "ec2:DeleteSnapshot",
+      "ec2:DescribeInstances",
+      "ec2:CreateKeyPair",
+      "ec2:DescribeRegions",
+      "ec2:CreateImage",
+      "ec2:CopyImage",
+      "ec2:ModifyImageAttribute",
+      "ec2:DescribeSnapshots",
+      "ec2:DeleteVolume",
+      "ec2:ModifySnapshotAttribute",
+      "ec2:CreateSecurityGroup",
+      "ec2:DescribeVolumes",
+      "ec2:CreateSnapshot",
+      "ec2:ModifyInstanceAttribute",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DetachVolume",
+      "ec2:TerminateInstances",
+      "ec2:DescribeTags",
+      "ec2:CreateTags",
+      "ec2:RegisterImage",
+      "ec2:RunInstances",
+      "ec2:StopInstances",
+      "ec2:DescribeSecurityGroups",
+      "ec2:CreateVolume",
+      "ec2:DescribeImages",
+      "ec2:GetPasswordData",
+      "ec2:DescribeImageAttribute",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DescribeSubnets",
+      "ec2:DeleteKeyPair"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+# The EC2 policies for our IAM user that lets the user do the actions needed
+# by packer to build AMIs.
+resource "aws_iam_user_policy" "ec2_packer_policy" {
+  count = var.add_packer_permissions ? 1 : 0
+
+  user   = aws_iam_user.user.id
+  policy = data.aws_iam_policy_document.ec2_packer_doc.json
+}
