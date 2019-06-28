6b63539cde23fc35a5a01a6646b78d150d449e2d
Dave Redmin <david.redmin@beta.dhs.gov>
2019-06-28 14:02:30 -0400

    Alphabetize EC2 roles and fix a grammatical error

--- main.tf
+++ main.tf
@@ -56,44 +56,44 @@ data "aws_iam_policy_document" "ec2_packer_doc" {
     actions = [
       "ec2:AttachVolume",
       "ec2:AuthorizeSecurityGroupIngress",
-      "ec2:DeregisterImage",
-      "ec2:DeleteSnapshot",
-      "ec2:DescribeInstances",
-      "ec2:CreateKeyPair",
-      "ec2:DescribeRegions",
-      "ec2:CreateImage",
       "ec2:CopyImage",
-      "ec2:ModifyImageAttribute",
-      "ec2:DescribeSnapshots",
-      "ec2:DeleteVolume",
-      "ec2:ModifySnapshotAttribute",
+      "ec2:CreateImage",
+      "ec2:CreateKeyPair",
       "ec2:CreateSecurityGroup",
-      "ec2:DescribeVolumes",
       "ec2:CreateSnapshot",
-      "ec2:ModifyInstanceAttribute",
-      "ec2:DescribeInstanceStatus",
-      "ec2:DetachVolume",
-      "ec2:TerminateInstances",
-      "ec2:DescribeTags",
       "ec2:CreateTags",
+      "ec2:CreateVolume",
+      "ec2:DeleteKeyPair",
+      "ec2:DeleteSecurityGroup",
+      "ec2:DeleteSnapshot",
+      "ec2:DeleteVolume",
+      "ec2:DeregisterImage",
+      "ec2:DescribeImageAttribute",
+      "ec2:DescribeImages",
+      "ec2:DescribeInstances",
+      "ec2:DescribeInstanceStatus",
+      "ec2:DescribeRegions",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSnapshots",
+      "ec2:DescribeSubnets",
+      "ec2:DescribeTags",
+      "ec2:DescribeVolumes",
+      "ec2:DetachVolume",
+      "ec2:GetPasswordData",
+      "ec2:ModifyImageAttribute",
+      "ec2:ModifyInstanceAttribute",
+      "ec2:ModifySnapshotAttribute",
       "ec2:RegisterImage",
       "ec2:RunInstances",
       "ec2:StopInstances",
-      "ec2:DescribeSecurityGroups",
-      "ec2:CreateVolume",
-      "ec2:DescribeImages",
-      "ec2:GetPasswordData",
-      "ec2:DescribeImageAttribute",
-      "ec2:DeleteSecurityGroup",
-      "ec2:DescribeSubnets",
-      "ec2:DeleteKeyPair"
+      "ec2:TerminateInstances",
     ]
 
     resources = ["*"]
   }
 }
 
-# The EC2 policies for our IAM user that lets the user do the actions needed
+# The EC2 policies for our IAM user that let the user do the actions needed
 # by packer to build AMIs.
 resource "aws_iam_user_policy" "ec2_packer_policy" {
   count = var.add_packer_permissions ? 1 : 0
