0ad626489ebbfc48c9aede3afee9cc9adb22951f
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-11 10:56:29 -0500

    Update example to include providers and new variable for images_account_id

--- examples/read_a_single_parameter/main.tf
+++ examples/read_a_single_parameter/main.tf
@@ -1,16 +1,30 @@
-# Configure AWS
+# Default AWS provider for the Images account
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
+  profile = "cool-images-provisionaccount"
+}
+
+# AWS provider for the Users account
+provider "aws" {
+  region  = "us-east-1"
+  profile = "cool-users-provisionaccount"
+  alias   = "users"
 }
 
 module "iam_user" {
   source = "github.com/cisagov/molecule-packer-ci-iam-user-tf-module"
 
-  add_packer_permissions = false
-  ssm_parameters         = ["/github/oauth_token"]
-  user_name              = "test-ansible-role-cyhy-core"
+  providers = {
+    aws       = aws
+    aws.users = aws.users
+  }
+
+  images_account_id = "111111111111"
+  ssm_parameters    = ["/github/oauth_token"]
+
+  user_name = "test-ansible-role-cyhy-core"
   tags = {
-    Team        = "NCATS OIS - Development"
+    Team        = "VM Fusion - Development"
     Application = "ansible-role-cyhy-core testing"
   }
 }
