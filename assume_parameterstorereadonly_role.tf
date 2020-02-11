efe0b92cb18323395437b6769de2fc2e4c7322c4
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-11 10:40:42 -0500

    Add policy allowing assumption of the custom ParameterStoreReadOnly role to the user created by this module

---
+++ assume_parameterstorereadonly_role.tf
@@ -0,0 +1,23 @@
+# IAM policy document that allows assumption of the ParameterStoreReadOnly
+# role (in the Images account) for this user
+data "aws_iam_policy_document" "assume_parameterstorereadonly_role_doc" {
+  statement {
+    effect = "Allow"
+
+    actions = ["sts:AssumeRole"]
+
+    resources = [
+      aws_iam_role.parameterstorereadonly_role.arn
+    ]
+  }
+}
+
+# The IAM policy allowing this user to assume their custom
+# ParameterStoreReadOnly role in the Images account
+resource "aws_iam_user_policy" "assume_parameterstorereadonly" {
+  provider = aws.users
+
+  name   = "Images-Assume${local.parameterstorereadonly_role_name}"
+  user   = aws_iam_user.user.name
+  policy = data.aws_iam_policy_document.assume_parameterstorereadonly_role_doc.json
+}
