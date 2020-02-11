fcda4a1f3208230ed481b5d2fe5879ffb78c36a5
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-11 10:26:24 -0500

    Create policy doc that allows roles to be assumed by the user we are creating with this module (and no one else)

---
+++ assume_role_policy_doc.tf
@@ -0,0 +1,20 @@
+# ------------------------------------------------------------------------------
+# Create an IAM policy document that allows the user created by this module
+# to assume the role that this policy is associated with.
+# ------------------------------------------------------------------------------
+
+# Policy allowing role to be assumed by this user
+data "aws_iam_policy_document" "assume_role_doc" {
+  statement {
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type = "AWS"
+      identifiers = [
+        aws_iam_user.user.arn
+      ]
+    }
+  }
+}
