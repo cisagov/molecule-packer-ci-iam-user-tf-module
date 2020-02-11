3875af90b1f78596999eaa7d1100ebab38e6d348
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-11 10:22:28 -0500

    Add new variables for AMIWrite role name/description, Images account ID, and ParameterStoreReadOnly role name/description

--- variables.tf
+++ variables.tf
@@ -25,6 +25,31 @@ variable "add_packer_permissions" {
   default     = false
 }
 
+variable "amiwrite_role_description" {
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows all of the EC2 actions needed to create an AMI.  Note that a \"%s\" in this value will get replaced with the user_name variable."
+  default     = "Allows access to all of the EC2 actions needed to create an AMI for %s."
+}
+
+variable "amiwrite_role_name" {
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows all of the EC2 actions needed to create an AMI.  Note that a \"%s\" in this value will get replaced with the user_name variable."
+  default     = "EC2AMIWrite-%s"
+}
+
+variable "images_account_id" {
+  description = "The AWS account ID containing the SSM parameter store that the IAM user needs to read from and also where the user will be allowed to create images (if add_packer_permissions is true); if not provided, the current calling account ID is used"
+  default     = ""
+}
+
+variable "parameterstorereadonly_role_description" {
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows read-only access to the specified SSM ParameterStore parameters.  Note that a \"%s\" in this value will get replaced with the user_name variable."
+  default     = "Allows read-only access to SSM ParameterStore required for %s."
+}
+
+variable "parameterstorereadonly_role_name" {
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows read-only access to SSM ParameterStore.  Note that a \"%s\" in this value will get replaced with the user_name variable."
+  default     = "ParameterStoreReadOnly-%s"
+}
+
 variable "tags" {
   type        = map(string)
   description = "Tags to apply to all AWS resources created"
