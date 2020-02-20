8d60b94f594142a4f5913082ac7262bb1e97ddec
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-20 18:25:49 -0500

    Allow the new user to assume the new custom role that allows creation of AMIs and reading the specified parameter store parameters

--- assume_ec2amicreate_role.tf
+++ assume_ec2amicreate_role.tf
@@ -9,17 +9,18 @@ data "aws_iam_policy_document" "assume_ec2amicreate_role_doc" {
     actions = ["sts:AssumeRole"]
 
     resources = [
-      "arn:aws:iam::${var.images_account_id}:role/${var.ec2amicreate_role_name}"
+      aws_iam_role.ec2amicreate_role[count.index].arn
     ]
   }
 }
 
+
 # The IAM policy allowing this user to assume their custom
 # EC2AMICreate role in the Images account
-resource "aws_iam_user_policy" "assume_ec2miwrite" {
+resource "aws_iam_user_policy" "assume_ec2amicreate" {
   count = var.add_packer_permissions ? 1 : 0
 
-  name   = "Images-Assume${var.ec2amicreate_role_name}"
+  name   = "Images-Assume${local.ec2amicreate_role_name}"
   user   = aws_iam_user.user.name
   policy = data.aws_iam_policy_document.assume_ec2amicreate_role_doc[count.index].json
 }
