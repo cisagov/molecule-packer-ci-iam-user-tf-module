c97aca0e00984ce9dafd0eb984b239692d095c99
Dave Redmin <david.redmin@beta.dhs.gov>
2020-02-20 18:20:39 -0500

    Add a new provider that can provision roles that are able to create AMIs (EC2AMICreate-* roles)

--- providers.tf
+++ providers.tf
@@ -1,4 +1,10 @@
-# This is the provider that is used to create the role and policy that can
+# This is the provider that is used to create the role that can
+# create AMIs inside the Images account
+provider "aws" {
+  alias = "images-ProvisionEC2AMICreateRoles"
+}
+
+# This is the provider that is used to create the policy that can
 # read Parameter Store parameters inside the Images account
 provider "aws" {
   alias = "images-ProvisionParameterStoreReadRoles"
